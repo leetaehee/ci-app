a2fcc768d71d353311cbcbde827623a1e4d235e3
leetaehee <lastride25@naver.com>
2024-07-28 14:42:05 +0900

    디버그 테스트 종료 후 복구

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,12 +6,6 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        $a = 3;
-        $b = 3;
-        $c = $a + $b;
-
-        echo $a . " + " .  $b . " = " . $c;
-
         return view('welcome_message');
     }
 }
